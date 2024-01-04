a512ec14b9a2cc708a6750b67bdd617cd18fb834
Nazar Chaban <134311145+Vivern0@users.noreply.github.com>
2024-01-04 20:21:35 +0200

    New features

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from collections import UserDict
 from datetime import datetime
+from pickle import dump, load
+
+
+class Connection:
+    def __init__(self, adr_book):
+        self.adr_book = adr_book
+
+    def __enter__(self):
+        self.adr_book._load_data()
+        print('Connection: data loaded')
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.adr_book._save_data()
+        print('Connection: data saved')
 
 
 class Field:
@@ -75,7 +89,10 @@ class Record:
         return None
 
     def add_birthday(self, birthday):
-        self.birthday.value = birthday
+        if self.birthday.value is None:
+            self.birthday.value = birthday
+        else:
+            raise ValueError('Birthday is already set')
 
     def days_to_birthday(self):
         if self.birthday.value is not None:
@@ -94,7 +111,7 @@ class Record:
     def __str__(self):
         if self.birthday.value is None:
             return f"Contact name: {self.name.value}; " \
-                    f"phones: {', '.join(p.value for p in self.phones)}."
+                   f"phones: {', '.join(p.value for p in self.phones)}."
 
         return f"Contact name: {self.name.value}; " \
                f"phones: {', '.join(p.value for p in self.phones)}; " \
@@ -106,7 +123,20 @@ class AddressBook(UserDict):
         self.data[user.name.value] = user
 
     def find(self, name):
-        return self.data.get(name, None)
+        return self.data.get(name)
+
+    def find_mathes(self, info):
+        if info.isdecimal():
+            temp = []
+            for value in self.data.values():
+                for phone in value.phones:
+                    if info in phone.value:
+                        temp.append(str(value))
+            return temp
+        elif info.isalpha():
+            return [str(p) for p in self.data.values() if info in p.name.value]
+        else:
+            raise ValueError('Wrong input format')
 
     def delete(self, name):
         if self.data.get(name) is not None:
@@ -125,71 +155,98 @@ class AddressBook(UserDict):
             start += n
             step += n
 
+    def _save_data(self):
+        with open('data.bin', 'wb') as fd:
+            dump(self.data, fd)
+
+    def _load_data(self):
+        try:
+            with open('data.bin', 'rb') as fd:
+                self.data = load(fd)
+        except Exception:
+            self.data = dict()
 
 if __name__ == '__main__':
     # Створення нової адресної книги
     book = AddressBook()
 
-    # Створення запису для John
-    john_record = Record("John")
-    john_record.add_phone("1234567890")
-    john_record.add_phone("5555555555")
+    with Connection(book):
+        # Створення запису для John
+        john_record = Record("John")
+        john_record.add_phone("1234567890")
+        john_record.add_phone("5555555555")
 
-    # Додавання запису John до адресної книги
-    book.add_record(john_record)
+        # Додавання запису John до адресної книги
+        book.add_record(john_record)
 
-    # Створення та додавання нового запису для Jane
-    jane_record = Record("Jane", '1111111111', '2222222222',
-                         birthday="01.01.2000")
-    jane_record.add_phone("9876543210")
-    book.add_record(jane_record)
+        # Створення та додавання нового запису для Jane
+        jane_record = Record("Jane", '1111111111', '2222222222',
+                            birthday="01.01.2000")
+        jane_record.add_phone("9876543210")
+        book.add_record(jane_record)
 
-    # Виведення всіх записів у книзі
-    for record in book.data.values():
-        print(record)
-    print('-'*50)
+        # Виведення всіх записів у книзі
+        for record in book.data.values():
+            print(record)
+        print('-'*50)
 
-    # Знаходження та редагування телефону для John
-    john = book.find("John")
-    john.edit_phone("1234567890", "1112223333")
+        # Знаходження та редагування телефону для John
+        john = book.find("John")
+        john.edit_phone("1234567890", "1112223333")
 
-    print(john)  # Виведення: Contact name: John, phones: 1112223333; 555555555
-    print('-'*50)
+        print(john)  # Виведення: Contact name: John, phones: 1112223333; 555555555
+        print('-'*50)
 
-    # Пошук конкретного телефону у записі John
-    found_phone = john.find_phone("5555555555")
-    print(f"{john.name}: {found_phone}")  # Виведення: John: 5555555555
-    print('-'*50)
+        # Пошук конкретного телефону у записі John
+        found_phone = john.find_phone("5555555555")
+        print(f"{john.name}: {found_phone}")  # Виведення: John: 5555555555
+        print('-'*50)
 
-    # Видалення запису Jane
-    book.delete("Jane")
+        # Видалення запису Jane
+        book.delete("Jane")
 
-    for record in book.data.values():
-        print(record)
-    print('-'*50)
+        for record in book.data.values():
+            print(record)
+        print('-'*50)
 
-    # Виведення днів до наступного дня народження John
-    print(john_record.days_to_birthday())
+        # Виведення днів до наступного дня народження John
+        print(john_record.days_to_birthday())
 
-    # Редагування дня народження John
-    john_record.add_birthday("01.01.2000")
-    print(john_record)
+        # Редагування дня народження John
+        john_record.add_birthday("01.01.2000")
+        print(john_record)
 
-    # Виведення днів до наступного дня народження John
-    print(john_record.days_to_birthday())
-    print('-'*50)
+        # Виведення днів до наступного дня народження John
+        print(john_record.days_to_birthday())
+        print('-'*50)
 
-    # Додавання 10 нових записів для тесту ітератора AddressBook
-    rec_1 = Record("Name1", "1234567890", birthday="01.01.2000")
-    book.add_record(rec_1)
-    rec_2 = Record("Name2", "1234567890", birthday="01.01.2000")
-    book.add_record(rec_2)
-    rec_3 = Record("Name3", "1234567890", birthday="01.01.2000")
-    book.add_record(rec_3)
-    rec_4 = Record("Name4", "1234567890", birthday="01.01.2000")
-    book.add_record(rec_4)
+        # Додавання нових записів для тесту ітератора AddressBook
+        rec_1 = Record("Name1", "1234567890", birthday="01.01.2000")
+        book.add_record(rec_1)
+        rec_2 = Record("Name2", "1234567890", birthday="01.01.2000")
+        book.add_record(rec_2)
+        rec_3 = Record("Name3", "1234567890", birthday="01.01.2000")
+        book.add_record(rec_3)
+        rec_4 = Record("Name4", "1234567890", birthday="01.01.2000")
+        book.add_record(rec_4)
 
-    # Виведення записів по 3
-    records_per_page = book.iterator(3)
-    for page in records_per_page:
-        print(page, end='\n\n')
+        # Виведення записів по 3
+        records_per_page = book.iterator(3)
+        for page in records_per_page:
+            print(page, end='\n\n')
+
+        print('-'*50)
+        print(book.find_mathes('Name'))
+
+        print('-'*50)
+        print(book.find_mathes('1234567'))
+
+    print('\nNew connection\n')
+
+    # Тест збереження даних
+    book2 = AddressBook()
+    with Connection(book2):
+        records_per_page = book.iterator(3)
+        for page in records_per_page:
+            print(page, end='\n\n')
+    
